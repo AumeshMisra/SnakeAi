65c81623dda850cd510ee8ed2e3afa92fea0682a
Aumesh Misra <aumeshmisra98@gmail.com>
2023-11-02 18:36:33 -0500

    adding game over screen + out of bounds

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 pygame.init()
-dis = pygame.display.set_mode((400, 400))
+
+# Setting up variables
+width: int = 400
+height: int = 400
+x1 = width/2
+y1 = height/2
+x1_change = 0
+y1_change = 0
+
+# Colors
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255, 255, 255)
+
+dis = pygame.display.set_mode((width, height))
 pygame.display.update()
 pygame.display.set_caption('Snake game by Aumesh')
 game_over = False
 
-blue = (0, 0, 255)
-red = (255, 0, 0)
-white = (255, 255, 255)
-x1 = 200
-y1 = 200
-x1_change = 0
-y1_change = 0
-clock = pygame.time.Clock()
 
+def displayTitle():
+    fontTitle = pygame.font.SysFont("arial", 24)
+    textTitle = fontTitle.render("Game Over", True, red)
+    rectTitle = textTitle.get_rect(center=dis.get_rect().center)
+    dis.blit(textTitle, rectTitle)
+    pygame.display.update()
+
+
+clock = pygame.time.Clock()
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,12 +46,19 @@ while not game_over:
             elif event.key == pygame.K_DOWN:
                 y1_change = 10
                 x1_change = 0
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, blue, [x1, y1, 10, 10])
-    pygame.display.update()
-    clock.tick(30)
+    if x1 <= 0 or x1 >= width:
+        displayTitle()
+        game_over = True
+    elif y1 <= 0 or y1 >= height:
+        displayTitle()
+        game_over = True
+    else:
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        pygame.draw.rect(dis, blue, [x1, y1, 10, 10])
+        pygame.display.update()
+        clock.tick(30)
 
-pygame.quit()
-quit()
+# pygame.quit()
+# quit()
